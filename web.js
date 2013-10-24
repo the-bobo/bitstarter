e5f1080fbae8f3997552aa25cec72cd1b76b6637
the-bobo <the.bobo@gmail.com>
2013-10-24 17:12:15 +0000

    finally working

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express');
-var fs = require('fs');
+//var fs = require('fs');
 
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
 
-var buffer = new Buffer(fs.readFileSync("index.html"));
-var thing = buffer.toString;
+var hello = fs.readFileSync("index.html").toString();
 
 app.get('/', function(request, response) {
-  response.send(thing);
+  response.send(hello);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
