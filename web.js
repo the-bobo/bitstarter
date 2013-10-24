372af04d6c16bdf430adfb513ca1a5ff86548659
the-bobo <the.bobo@gmail.com>
2013-10-24 15:37:33 +0000

    Updated web.js first try for the buffer thing

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buffer = new Buffer(128);
+buffer.write(fs.readFileSync(index.html, utf-8));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
