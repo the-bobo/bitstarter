64646e379701d570b09b03d2f1e047da0252f830
the-bobo <the.bobo@gmail.com>
2013-10-24 15:53:57 +0000

    changed again

--- web.js
+++ web.js
@@ -5,9 +5,10 @@ app.use(express.logger());
 var buffer = new Buffer(128);
 // buffer.write(fs.readFileSync(index.html, utf-8));
 buffer.write("String");
+var thing = buffer.toString;
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(thing);
 });
 
 var port = process.env.PORT || 5000;
